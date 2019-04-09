f6f7f60fab67ce3fb90c8b0d96062862535d5be0
ssh <kathishivaram@gmail.com>
2019-04-09 11:53:21 +0530

    adding corelocation for accessing user location data

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -1,10 +1,19 @@
 import UIKit
+import CoreLocation
 
-
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController,  CLLocationManagerDelegate{
+    
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+    let APP_ID = "4fad235f15c99da34ab7c3464811fcf3"
+    
+    let locationManager = CLLocationManager()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
     }
 }
 
